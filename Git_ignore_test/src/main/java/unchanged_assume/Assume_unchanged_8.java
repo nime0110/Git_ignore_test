a2743b9ba9fdf7d6787c7cd649183a0c27ee855c
nime0110 <nime0110@naver.com>
2024-05-10 11:31:24 +0900

    Assume_unchanged_8 클래스 완성

--- Git_ignore_test/src/main/java/unchanged_assume/Assume_unchanged_8.java
+++ Git_ignore_test/src/main/java/unchanged_assume/Assume_unchanged_8.java
@@ -30,6 +30,8 @@ public class Assume_unchanged_8 {
 	*/
 		System.out.println("==== Assume_unchanged_8.java 파일입니다. ===="); 
 		System.out.println("==== Assume_unchanged_8.java 파일 내용 변경 ===="); 
+		System.out.println("==== Assume_unchanged_8.java 파일 2차 수정 ===="); 
+		System.out.println("==== Assume_unchanged_8.java 파일 마지막 완료 ===="); 
 
 	}
 
